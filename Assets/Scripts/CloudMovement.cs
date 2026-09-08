0179082403fd6dcb8d646b665b00c980f8bbf398
90C68SQHX9VSLMX\unitylhr011 <raffay005.ar@gmail.com>
2026-09-08 14:42:23 +0800

    adding textures to 7th heaven

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class CloudMovement : MonoBehaviour
 {
     Animator animator;
+    public float speed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,6 +16,7 @@ public class CloudMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             animator.SetBool("isMoving", true);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
         if (!Input.GetKey(KeyCode.W))
